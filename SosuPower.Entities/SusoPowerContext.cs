b5b49077375b3f0a12e98a0f10ab9064108e8691
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-05-14 14:34:21 +0200

    moved entity types to separate files

--- SosuPower.Entities/SusoPowerContext.cs
+++ SosuPower.Entities/SusoPowerContext.cs
@@ -6,26 +6,4 @@ namespace SosuPower.Entities
     {
 
     }
-
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<EmployeeTask> Tasks { get; set; }
-    }
-
-    public class Task
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public ICollection<EmployeeTask> Employees { get; set; }
-    }
-
-    public class EmployeeTask
-    {
-        public int EmployeeId { get; set; }
-        public Employee Employee { get; set; }
-        public int TaskId { get; set; }
-        public Task Task { get; set; }
-    }
 }
---
+++ SosuPower.Entities/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace SosuPower.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<EmployeeTask> Employees { get; set; }
+    }
+}
