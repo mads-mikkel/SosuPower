d3e67f4c5d64e2f230b54f9980cae52a51f4ebbe
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-05-16 13:12:47 +0200

    modified OnModelCreated

--- SosuPower.Entities/SusoPowerContext.cs
+++ SosuPower.Entities/SusoPowerContext.cs
@@ -4,21 +4,18 @@ namespace SosuPower.Entities
 {
     public class SosuPowerContext: DbContext
     {
-        public SosuPowerContext(DbContextOptions<SosuPowerContext> options) : base(options)
+        public SosuPowerContext(DbContextOptions<SosuPowerContext> options) 
+            : base(options)
         {
 
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>()
-                .HasMany(e => e.Tasks)
-                .WithMany(t => t.Employees);
-
+            
 
             base.OnModelCreating(modelBuilder);
         }
-
 
         DbSet<Employee> Employees { get; set; }
         DbSet<Task> Tasks { get; set; }
