f81eff649e91e4783fcbf7a56d530570b576116b
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-05-22 09:26:32 +0200

    added repository

---
+++ SosuPower.DataAccess/IRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SosuPower.DataAccess
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetAll();
+        T GetBy(int id);
+    }
+
+    public class Repository<T>: IRepository<T>
+    {
+        private readonly SosuPowerContext sosuPowerContext;
+
+        public Repository(SosuPowerContext sosuPowerContext)
+        {
+            this.sosuPowerContext = sosuPowerContext;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
