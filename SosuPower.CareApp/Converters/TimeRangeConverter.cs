b3031220f124583d0c135ab852e50e7b84b7be95
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-05-29 11:33:25 +0200

    attempt to make multibinding

---
+++ SosuPower.CareApp/Converters/TimeRangeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace SosuPower.CareApp.Converters
+{
+    public class TimeRangeConverter: IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values[0] is DateTime start && values[1] is DateTime end)
+            {
+                return $"{start:HH.mm} - {end:HH.mm}";
+            }
+            return string.Empty;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
